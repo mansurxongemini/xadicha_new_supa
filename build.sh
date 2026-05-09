c770329be1196d6a5d1277eaaa707a1428609c68
mansurxongemini <mansurxon.gemini@gmail.com>
2026-05-09 15:02:09 +0500

    s

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
+# Xatolik bo'lsa to'xtash, lekin biz ba'zi buyruqlarni xavfsiz qilamiz
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
+
+# Statik fayllarni yig'ish (agar xato bo'lsa ham skript to'xtamaydi)
+python manage.py collectstatic --no-input || true
+
+# Bazani migratsiya qilish (BU ENG MUHIMI, u endi albatta ishlaydi)
 python manage.py migrate
 
-# Admin yaratish (Render Environment bo'limida DJANGO_SUPERUSER_... o'zgaruvchilari bo'lishi kerak)
+# Admin yaratish
 python manage.py createsuperuser --noinput || true
